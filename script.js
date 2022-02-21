5d2841a43a370a1c018d29ecfcab60a6174c6123
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-21 15:59:20 +0600

    changes

--- script.js
+++ script.js
@@ -11,30 +11,56 @@
 //     this.value = "Bye";
 // })
 
-document.querySelector('input[type="text"]')
-.addEventListener('focus', function() {
-    if(!this.value) {
-        this.style.borderColor = "yellow";
-    }
+// document.querySelector('input[type="text"]')
+// .addEventListener('focus', function() {
+//     if(!this.value) {
+//         this.style.borderColor = "yellow";
+//     }
     
-});
+// });
 
-document.querySelector('input[type="text]')
-addEventListener('blur', function() {
-    this.style.borderColor = "green";
-});
+// document.querySelector('input[type="text]')
+// addEventListener('blur', function() {
+//     this.style.borderColor = "green";
+// });
 
-document.querySelector('input[type="text]')
-addEventListener('change', function() {
-    alert('changed');
-});
+// document.querySelector('input[type="text]')
+// addEventListener('change', function() {
+//     alert('changed');
+// });
 
-document.querySelector('input[type="text"')
-addEventListener('change', function() {
-    if(this.checked) {
-        this.alert('You are employed');
+// document.querySelector('input[type="text"')
+// addEventListener('change', function() {
+//     if(this.checked) {
+//         this.alert('You are employed');
+//     }
+//     else {
+//         alert ('You are umemployed');
+//     }
+// });
+
+document.querySelector('select')
+.addEventListener('input', function() {
+    console.log(this.value);
+    if (this.value =='Bishkek') {
+        alert('Chui');
     }
-    else {
-        alert ('You are umemployed');
+    else if (this.value =='Karakol') {
+        alert('Issyk-Kul');
     }
-});
+    else if (this.value =='Osh') {
+        alert('Osh');
+    }
+    else if (this.value =='Jalal-Abad') {
+        alert('Jalal-Abad');
+    }
+    else if (this.value =='Naryn') {
+        alert('Naryn');
+    }
+    else if (this.value =='Talas') {
+        alert('Talas');
+    }
+    else if (this.value =='Batken') {
+        alert('Batken');
+    }
+})
